83cd689b1410901111d4267c104c5e2c0f7747b1
Carlos Gregorio <carlosgregorio3@gmail.com>
2025-05-08 10:59:28 +0200

    Arreglamos los colliders de los arboles y con ello el movimiento del personaje

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,21 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
-    private Vector2 vector;
+    private float verticalInput = 0f;
+    private float horizontalInput = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = Vector2.zero;
     }
 
     void Update()
     {
-        vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(horizontalInput * speed, verticalInput * speed);
     }
 
-    void FixedUpdate()
-    {
-        rb.linearVelocity = vector * speed;
-    }
+   
 }
